8ff51fd09b5191e6b506525e2b0aefffd8e40534
Edd Smith <edd@bigbrandideas.co.uk>
2017-11-22 11:34:27 +0000

    added mobile nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,22 @@ $(document).ready(function(){
 	function check_nav_display(){
 		if ( $(document).scrollTop() > $('.js-nav-check').offset().top + $('.js-nav-check').height() ) {
 			$('.js-nav').css('opacity','1');
-		}else{
+		} else {
 			$('.js-nav').css('opacity','0');
 		}
 	}
 
 	//gets width for banner nav bar
 	$('.js-nav-check').css('width','' + ($('.js-title').width() - 16) + 'px');
+
+	//bottom nav display/hide
+	$('.bottom_nav').click(function(){
+
+		if ( $('.bottom-nav__nav').hasClass('js-bottom-nav-show') ) {
+			$('.bottom-nav__nav').removeClass('js-bottom-nav-show');
+		} else {
+			$('.bottom-nav__nav').addClass('js-bottom-nav-show');
+		}
+	})
 
 })
